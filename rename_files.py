4d696b6da66eac6529f307d34f9be1393c3aa7c4
Artur Kornelski <51404198+Artkor1@users.noreply.github.com>
2023-03-23 16:21:11 +0100

    Add files via upload

--- rename_files.py
+++ rename_files.py
@@ -2,10 +2,10 @@ import os
 
 
 
-path = r"C:\Users\Artkor1\PycharmProjects\Dice Reader\d6d8d10d12_images_dataset_temp\test"
+path = r"C:\Users\xyz\PycharmProjects\Dice Reader\dataset"
 files = os.listdir(path)
 
-counter = 3187
+counter = 0
 for file in files:
     old_name = path + "\\" + file
     new_name = path + "\\dice" + str(counter) + ".jpg"
